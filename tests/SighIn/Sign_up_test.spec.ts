cadb8b40c4323df1dc620b15510959dcd64ff79a
Alex Galaiev <sashagalaievsecond@gmail.com>
2024-09-09 13:22:19 +0300

    remove diblicaite tests

--- tests/SighIn/Sign_up_test.spec.ts
+++ tests/SighIn/Sign_up_test.spec.ts
@@ -8,40 +8,6 @@ import { SighIn } from "../../pageObjects/SighIn/SignInPage";
 test.describe("Naga Capital. Sigh up page", async()=>{
     let SighUpPage_localization = "/pageObjects/localization/SignUpPage.json"
 
-
-    //need to check for dublication for entity redirection
-    test.skip(`@24931 Change brand popup from NS to NM`, async({page,NagaCapital, NagaMarkets, NMCountry})=>{
-        let localizationText = new getLocalization('/pageObjects/localization/SignUpPage.json')
-        let sighUp = new SighUp(page);
-        let changeBrandPopup = new ChangeBrandPopup(page);
-        await test.step('Open platform and try to create user from not regualted country', async()=>{
-            await sighUp.goto(NagaCapital, "register")
-            await sighUp.createCFDUser(NMCountry);
-        })
-        await test.step('Check change brand popup and check redirect to correct platform', async()=>{
-            expect(await changeBrandPopup.getPopupHeaderText()).toEqual(await localizationText.getLocalizationText("SighUp_redirectToOppositeBrand_head"));
-            expect(await changeBrandPopup.getPopupText()).toEqual(await localizationText.getLocalizationText("SighUp_redirectToOppositeBrand_text"));
-            await changeBrandPopup.proceedRegistration();
-            expect(await page.url()).toContain(NagaMarkets)
-        })
-    });
-
-    test.skip(`@25141 Change brand popup from NM to NS`, async({page, NagaCapital, NagaMarkets, NSCountry})=>{
-        let localizationText = new getLocalization('/pageObjects/localization/NagaMarkets_SighUp.json')
-        let sighUp = new SighUp(page);
-        let changeBrandPopup = new ChangeBrandPopup(page);
-        await test.step('Open platform and try to create user from not regualted country', async()=>{
-            await sighUp.goto(NagaMarkets, "register")
-            await sighUp.create_NM_CFDUser(NSCountry);
-        })
-        await test.step('Check change brand popup and check redirect to correct platform', async()=>{
-            expect(await changeBrandPopup.getPopupHeaderText()).toEqual(await localizationText.getLocalizationText("SighUp_redirectToOppositeBrand_head"));
-            expect(await changeBrandPopup.getPopupText()).toEqual(await localizationText.getLocalizationText("SighUp_redirectToOppositeBrand_text"));
-            await changeBrandPopup.proceedRegistration();
-            expect(await page.url()).toContain(NagaCapital)
-        })
-    });
-
     type testRiskDisclaimer = {
         testRailId: string,
         brand: string,
