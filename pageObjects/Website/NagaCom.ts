c86dbc15d62ad37506b4c1c3dd9c0537061feaa5
AlexGalaiev <139119704+AlexGalaiev@users.noreply.github.com>
2024-10-15 12:33:54 +0300

    test (#118)

--- pageObjects/Website/NagaCom.ts
+++ pageObjects/Website/NagaCom.ts
@@ -2,11 +2,13 @@ import {Locator, Page } from "playwright";
 
 export class NagaCom{
     page: Page;
-    languageSwitcher: Locator
+    languageSwitcher: Locator;
+    redirectPopupText: Locator;
 
     constructor(page: Page){
         this.page = page
-        this.languageSwitcher = page.locator('#mantine-bbydtmfqy-target')
+        this.languageSwitcher = page.locator("//div[contains(@class, 'LanguageSelector_language-trigger')]").first()
+        this.redirectPopupText = page.locator("//button//span[text()='OK']//..//..//preceding-sibling::div[1]")
     }
 
     async checkTradeInstrument(nameOfInstrument: string){
@@ -35,5 +37,20 @@ export class NagaCom{
         let point = await this.page.locator(`#language_menu_item_${language}`)
         await point.click()
     }
-
+    async getRedirectAllertPopupText(){
+        return await this.redirectPopupText.textContent();
+    }
+    async acceptRedirectPopup(){
+        await this.page.locator("//button//span[text()='OK']").click();
+    }
+    async checkVisibileLanguage(language:string){
+        return await this.page.locator(`//span[text()='${language}']`).isVisible()
+    }
+    async openLanguages(){
+        await this.languageSwitcher.click();
+        await this.page.waitForTimeout(1000)
+    }
+    async getName(exampleName: string){
+        return await this.page.locator(`//span[text()='${exampleName}']`).first().textContent()
+    }
 }
--- tests/Website/WebsiteNagaCom.spec.ts
+++ tests/Website/WebsiteNagaCom.spec.ts
@@ -2,59 +2,92 @@ import { expect } from "@playwright/test";
 import { NagaCom } from "../../pageObjects/Website/NagaCom";
 import { VPN } from "../../pageObjects/Website/VPN"
 import {test} from "../../test-options"
+import { getLocalization } from "../../pageObjects/localization/getText";
 
 test.describe('Naga.com website', async()=>{
     
     type RedirectTypes = {
-        tradeInstrument: string;
-        nameOfTheButton: string;
-        redirectPageUrl: string;
+        testRailId: string
+        type: string;
+        buttonName: string;
+        redirectTo: string;
+        baseUrl: string;
     }
-    const testRedirectToNMParams: RedirectTypes[] = [
-        {tradeInstrument: 'trade', nameOfTheButton: 'Login', redirectPageUrl: 'https://nagamarkets.com/login'},
-        {tradeInstrument: 'trade', nameOfTheButton: 'Get started', redirectPageUrl: 'https://nagamarkets.com/register'},
-        {tradeInstrument: 'invest', nameOfTheButton: 'Login', redirectPageUrl: 'https://nagamarkets.com/login'},
-        {tradeInstrument: 'invest', nameOfTheButton: 'Get started', redirectPageUrl: 'https://nagamarkets.com/register'},
-        {tradeInstrument: 'crypto', nameOfTheButton: 'Login', redirectPageUrl: 'https://app.nagax.com/eu/login'},
-        {tradeInstrument: 'crypto', nameOfTheButton: 'Get started', redirectPageUrl: 'https://app.nagax.com/eu/register'},
-        {tradeInstrument: 'pay', nameOfTheButton: 'Get your card', redirectPageUrl: 'https://naga.com/eu/pay'},
+    const fromWebsiteToNM: RedirectTypes[] = [
+        {testRailId: '@25194', type: 'trade', buttonName: 'Login', baseUrl: 'https://naga.com/eu', redirectTo: 'https://nagamarkets.com/login'},
+        {testRailId: '@25194', type: 'trade', buttonName: 'Get started',baseUrl: 'https://naga.com/eu', redirectTo: 'https://nagamarkets.com/register'},
+        {testRailId: '@25194', type: 'invest', buttonName: 'Login', baseUrl: 'https://naga.com/eu',redirectTo: 'https://nagamarkets.com/login'},
+        {testRailId: '@25194', type: 'invest', buttonName: 'Get started', baseUrl: 'https://naga.com/eu',redirectTo: 'https://nagamarkets.com/register'},
+        {testRailId: '@25194', type: 'crypto', buttonName: 'Login', baseUrl: 'https://naga.com/eu', redirectTo: 'https://app.nagax.com/eu/login'},
+        {testRailId: '@25194', type: 'crypto', buttonName: 'Get started', baseUrl: 'https://naga.com/eu', redirectTo: 'https://app.nagax.com/eu/register'},
+        {testRailId: '@25194', type: 'pay', buttonName: 'Get your card', baseUrl: 'https://naga.com/eu', redirectTo: 'https://naga.com/eu/pay'},
+        {testRailId: '@25208', type: 'Trade', buttonName: 'Login', baseUrl: 'https://naga.com/en', redirectTo: 'https://nagamarkets.com/login'},
+        {testRailId: '@25208', type: 'Invest', buttonName: 'Login', baseUrl: 'https://naga.com/en', redirectTo: 'https://nagamarkets.com/login'},
+    
     ]
-    for(const{tradeInstrument, nameOfTheButton, redirectPageUrl}of testRedirectToNMParams){
-        test(`@25194 Redirect from Naga.com to NagaMarkets platform. From ${tradeInstrument} to ${nameOfTheButton} button`, async({proxyPage})=>{
+    for(const{testRailId, type, buttonName, redirectTo, baseUrl }of fromWebsiteToNM){
+        test(`${testRailId} Redirect from ${baseUrl} to NagaMarkets platform. From ${type} to ${buttonName} button`, {tag: ['@prodSanity', '@website-naga.com']}, async({proxyPage})=>{
             let website = new NagaCom(proxyPage)
             await test.step('Open website Naga.com', async()=>{
-                await website.open('https://naga.com/eu')
+                await website.open(`${baseUrl}`)
             })
-            await test.step(`Choose ${tradeInstrument}. Click ${nameOfTheButton}`, async()=>{
-                await website.checkTradeInstrument(`${tradeInstrument}`)
-                await website.clickBtn(`${nameOfTheButton}`)
-                expect(await website.checkUrl()).toContain(`${redirectPageUrl}`)
+            await test.step(`Choose ${type}. Click ${buttonName}`, async()=>{
+                await website.checkTradeInstrument(`${type}`)
+                await website.clickBtn(`${buttonName}`)
+                expect(await website.checkUrl()).toContain(`${redirectTo}`)
             })
         })
     }
     
-    const testRedirectToNSParams: RedirectTypes[] = [
-        {tradeInstrument: 'trade', nameOfTheButton: 'Login', redirectPageUrl: 'https://nagacap.com/login'},
-        {tradeInstrument: 'trade', nameOfTheButton: 'Get started', redirectPageUrl: 'https://nagacap.com/register'},
-        {tradeInstrument: 'invest', nameOfTheButton: 'Login', redirectPageUrl: 'https://nagacap.com/login'},
-        {tradeInstrument: 'invest', nameOfTheButton: 'Get started', redirectPageUrl: 'https://nagacap.com/register'},
-        {tradeInstrument: 'crypto', nameOfTheButton: 'Login', redirectPageUrl: 'https://app.nagax.com/eu/login'},
-        {tradeInstrument: 'crypto', nameOfTheButton: 'Get started', redirectPageUrl: 'https://app.nagax.com/eu/register'},
-        {tradeInstrument: 'pay', nameOfTheButton: 'Get your card', redirectPageUrl: 'https://naga.com/eu/pay'},
+    const fromWebsitetoNS: RedirectTypes[] = [
+        {testRailId: '@25195', type: 'trade', buttonName: 'Login', baseUrl: 'https://naga.com/eu', redirectTo: 'https://nagacap.com/login'},
+        {testRailId: '@25195', type: 'trade', buttonName: 'Get started', baseUrl: 'https://naga.com/eu', redirectTo: 'https://nagacap.com/register'},
+        {testRailId: '@25195', type: 'invest', buttonName: 'Login', baseUrl: 'https://naga.com/eu', redirectTo: 'https://nagacap.com/login'},
+        {testRailId: '@25195', type: 'invest', buttonName: 'Get started', baseUrl: 'https://naga.com/eu', redirectTo: 'https://nagacap.com/register'},
+        {testRailId: '@25195', type: 'crypto', buttonName: 'Login', baseUrl: 'https://naga.com/eu', redirectTo: 'https://app.nagax.com/eu/login'},
+        {testRailId: '@25195', type: 'crypto', buttonName: 'Get started', baseUrl: 'https://naga.com/eu',  redirectTo: 'https://app.nagax.com/eu/register'},
+        {testRailId: '@25195', type: 'pay', buttonName: 'Get your card', baseUrl: 'https://naga.com/eu', redirectTo: 'https://naga.com/eu/pay'},
+        {testRailId: '@25209', type: 'Trade', buttonName: 'Login', baseUrl:'https://naga.com/en', redirectTo: 'https://nagacap.com/login'},
+        {testRailId: '@25209', type: 'Trade', buttonName: 'Get started', baseUrl:'https://naga.com/en',  redirectTo: 'https://nagacap.com/register'},
+        {testRailId: '@25209', type: 'Invest', buttonName: 'Login', baseUrl:'https://naga.com/en', redirectTo: 'https://nagacap.com/login'},
+        {testRailId: '@25209', type: 'Invest', buttonName: 'Get started', baseUrl:'https://naga.com/en', redirectTo: 'https://nagacap.com/register'},
+   
+   
     ]
-    for(const{tradeInstrument, nameOfTheButton, redirectPageUrl}of testRedirectToNSParams){
-        test(`@25195 Redirect from Naga.com to Nagacapital platform. From ${tradeInstrument} to ${nameOfTheButton} button`, async({page})=>{
+    for(const{type, buttonName, redirectTo, testRailId, baseUrl}of fromWebsitetoNS){
+        test(`${testRailId} Redirect from ${baseUrl} to Nagacapital platform. From ${type} to ${buttonName} button`, {tag: ['@prodSanity', '@website-naga.com']},async({page})=>{
             let website = new NagaCom(page)
             await test.step('Open website Naga.com', async()=>{
-                await website.open('https://naga.com/eu')
+                await website.open(`${baseUrl}`)
             })
-            await test.step(`Choose ${tradeInstrument}. Click ${nameOfTheButton}`, async()=>{
-                await website.checkTradeInstrument(`${tradeInstrument}`)
-                await website.clickBtn(`${nameOfTheButton}`)
-                expect(await website.checkUrl()).toContain(`${redirectPageUrl}`)
+            await test.step(`Choose ${type}. Click ${buttonName}`, async()=>{
+                await website.checkTradeInstrument(`${type}`)
+                await website.clickBtn(`${buttonName}`)
+                expect(await website.checkUrl()).toContain(`${redirectTo}`)
             })})
     }
 
+    const fromENtoNMAllert: RedirectTypes[] = [
+        {testRailId: '@25210', type: 'Trade', buttonName: 'Get started', baseUrl:'https://naga.com/en', redirectTo: 'https://nagamarkets.com/register'},
+        {testRailId: '@25210', type: 'Invest', buttonName: 'Get started', baseUrl:'https://naga.com/en',  redirectTo: 'https://nagamarkets.com/register'},
+    ]
+    for(const{type, buttonName, redirectTo, testRailId, baseUrl}of fromENtoNMAllert){
+        test(`${testRailId} Redirect from ${baseUrl} to NagaMarkets. Redirect allert popup. From ${type} to ${buttonName} button`, {tag: ['@prodSanity', '@website-naga.com']},async({proxyPage})=>{
+            let website = new NagaCom(proxyPage)
+            let localization = new getLocalization('/pageObjects/localization/Website_NagaCom.json')
+            await test.step('Open website Naga.com', async()=>{
+                await website.open(`${baseUrl}`)
+            })
+            await test.step(`Choose ${type}. Click ${buttonName}`, async()=>{
+                await website.checkTradeInstrument(`${type}`)
+                await website.clickBtn(`${buttonName}`)
+            await test.step('Check redirect notification popup text. And redirect to Webtrader', async()=>{
+                expect(await website.getRedirectAllertPopupText()).toContain(await localization.getLocalizationText("RedirectAllertPopupText"))
+                await website.acceptRedirectPopup()
+            })
+                expect(await website.checkUrl()).toContain(`${redirectTo}`)
+            })})
+        }
 
     type languageTypes = {
         regulation: string;
@@ -63,22 +96,21 @@ test.describe('Naga.com website', async()=>{
     }
     const languageParameters: languageTypes[] = [
         {testRailId: '@25197', regulation: 'eu', languages: ['English (Europe)', 'Deutsch', 'Italiano', 'Español', 'Polski', 'Čeština', 'Nederlands', 'Português']},
-        {testRailId: '@25198', regulation: 'en', languages: ['English (Global)', 'Español (Latam)', 'Português', 'العربية', 'Bahasa Indonesia', '简化字', '繁體中文']},
+        {testRailId: '@25196', regulation: 'en', languages: ['English (Global)', 'Español (Latam)', 'Português', 'العربية', 'Bahasa Indonesia', '简化字', '繁體中文']},
     ]
     for(const{testRailId, regulation, languages}of languageParameters){
-        test(`${testRailId} Default languages via ${regulation}`, async({page})=>{
+        test(`${testRailId} Default languages via ${regulation}`,{tag: ['@prodSanity', '@website-naga.com']}, async({page})=>{
             let website = new NagaCom(page)
             await test.step('Open Naga.com website', async()=>{
                 await website.open(`https://naga.com/${regulation}`)
             })
             await test.step('Check visible language', async()=>{
-                for(let language in languages){
-                    
+                await website.openLanguages()
+                for(let index in languages){
+                    expect(await website.checkVisibileLanguage(languages[index])).toBeTruthy()
                 }
             })
         })
     }
-
     
-
 })
