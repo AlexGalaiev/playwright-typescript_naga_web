76176bed242a41ffd2bbe3670e4685446dedf404
AlexGalaiev <139119704+AlexGalaiev@users.noreply.github.com>
2024-11-28 12:07:25 +0200

    test (#164)

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig<TestOptions>({
     screenshot: "only-on-failure",
     trace:"off",
   },
-  globalSetup: process.env.CI
+  globalSetup: process.env.CI && process.env.TEST_RUN !== "schedule"
     ? require.resolve("./global-setup.ts")
     : undefined,
 
