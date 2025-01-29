b4061d6c4d10f267aa1b489ed874f280ad7440ad
Alex Galaiev <sashagalaievsecond@gmail.com>
2025-01-29 16:06:54 +0200

    test

--- pageObjects/common/testUserCredentials/randomUser.ts
+++ pageObjects/common/testUserCredentials/randomUser.ts
@@ -5,7 +5,7 @@ export class RandomUser {
 
     public getRandomUserEmail() {
         let momentTime = new Date();
-        let randomUser = `test_user_${momentTime.getTime()}@naga.com`;
+        let randomUser = `testUser_${momentTime.getTime()}@naga.com`;
         return randomUser;
     };
     public randomUserName(){
